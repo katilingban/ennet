78165a6d70dfd50106262ef7870a241677f91633
Ernest Guevarra <ernest@guevarra.io>
2020-07-10 22:31:49 +0100

    initial commit

---
+++ R/rennet.R
@@ -0,0 +1,22 @@
+################################################################################
+#
+#'
+#' Utilities to Extract Text Data From EN-NET
+#'
+#' en-net is the go to online forum for field practitioners requiring prompt
+#' technical advice for operational challenges for which answers are not readily
+#' accessible in current guidelines. The questions and the corresponding answers
+#' raised within en-net can provide insight into what the key topics of
+#' discussion are within the nutrition sector. This package provides utility
+#' functions for the extraction, processing and analysis of text data from the
+#' online forum.
+#'
+#' @docType package
+#' @keywords internal
+#' @name rennet
+#'
+#'
+#
+################################################################################
+"_PACKAGE"
+
