3f3023b5a05661e6ce44eac1704edea7413651fd
Ernest Guevarra <ernest@guevarra.io>
2020-07-11 22:04:53 +0100

    update tests

--- tests/testthat/test-get_themes.R
+++ tests/testthat/test-get_themes.R
@@ -12,3 +12,27 @@ test_that("output has 2 columns", {
 test_that("output has correct names", {
   expect_equal(names(x), c("themes", "links"))
 })
+
+themes <- get_themes()
+x <- get_theme_topics(link = themes$links[1])
+
+test_that("output is a tibble", {
+  expect_is(x, "tbl")
+})
+
+test_that("output has correct names", {
+  expect_true(all(names(x) %in% c("Theme", "Topic", "Views", "Author", "Posted", "Link")))
+})
+
+themes <- get_themes()
+x <- get_themes_topics(themes = themes[1:3, ])
+
+test_that("output is a tibble", {
+  expect_is(x, "tbl")
+})
+
+test_that("output has correct names", {
+  expect_true(all(names(x) %in% c("Theme", "Topic", "Views", "Author", "Posted", "Link", "Replies")))
+})
+
+
