2cafeb2b4ba674f21dc9720c8e94cd20e7e04d04
Ernest Guevarra <ernest@guevarra.io>
2020-07-11 13:37:01 +0100

    update functions

--- R/get_question.R
+++ R/get_question.R
@@ -1,7 +1,7 @@
 ################################################################################
 #
 #'
-#' Get the question for a particular topic
+#' Get the discussion and other details for a particular topic
 #'
 #' @param link URL for topic discussion
 #'
@@ -9,38 +9,130 @@
 #'
 #' @examples
 #' links <- get_theme_topics(link = get_themes()$links[4])
-#' get_topic_question(link = links$Link)
+#' get_topic_discussion(link = links$Link[1])
+#'
+#' @export
+#'
+#
+################################################################################
+
+get_topic_discussion <- function(link) {
+  ## Read the link
+  page <- xml2::read_html(x = link)
+
+  ## Get the theme
+  theme <- page %>%
+    rvest::html_nodes(css = "#pagebody p a") %>%
+    rvest::html_text() %>%
+    dplyr::first()
+
+  ## Get the topic
+  topic <- page %>%
+    rvest::html_nodes(css = "#pagebody h1") %>%
+    rvest::html_text()
+
+  ## Get the user
+  user <- page %>%
+    rvest::html_nodes(css = "#pagebody .post .data h3") %>%
+    rvest::html_text()
+
+  ## Get user code
+  userCode <- page %>%
+    rvest::html_nodes(css = "#pagebody .post .data h3 a") %>%
+    rvest::html_attr(name = "href") %>%
+    stringr::str_remove_all(pattern = "\\/|\\.aspx")
+
+  ##
+  details <- page %>%
+    rvest::html_nodes(css = "#pagebody .post .data p") %>%
+    rvest::html_text() %>%
+    matrix(ncol = 3, byrow = TRUE) %>%
+    data.frame()
+
+  ##
+  names(details) <- c("job", "role", "date_time")
+
+  ## Is is a question or an answer
+  type <- page %>%
+    rvest::html_nodes(css = "#pagebody .post") %>%
+    rvest::html_attr(name = "id") %>%
+    stringr::str_extract(pattern = "Question") %>%
+    stringr::str_to_lower() %>%
+    stringr::str_replace_na(replacement = "answer")
+
+  ## Code
+  code <- vector(mode = "character",
+                 length = page %>%
+                   rvest::html_nodes(css = "#pagebody .post") %>%
+                   length())
+
+  code[1] <- stringr::str_extract(string = link,
+                                  pattern = "[0-9]+")
+
+  if(length(code) > 1) {
+    ##
+    code1 <- page %>%
+      rvest::html_nodes(css = "#pagebody .post a") %>%
+      rvest::html_attr(name = "name")
+
+    code1 <- code1[stringr::str_detect(string = code1,
+                                       pattern = "answer")] %>%
+      stringr::str_extract_all(pattern = "[0-9]+", simplify = TRUE)
+
+    code1 <- code1[!is.na(code1)]
+
+    ##
+    code[seq(from = 2, to = length(code), by = 1)] <- code1
+  }
+
+  ## Post
+  post <- page %>%
+    rvest::html_nodes(css = "#pagebody .body") %>%
+    rvest::html_text()
+
+  ## Concatenate
+  discussion <- data.frame(theme, topic, user, userCode,
+                           details, type, code, post)
+
+  ## Convert to tibble
+  discussion <- tibble::tibble(discussion)
+
+  ## Return discussion
+  return(discussion)
+}
+
+
+################################################################################
+#
+#'
+#' Get the discussion and details of discussion for a set of topics
+#'
+#' @param links A tibble of topics containing URL of topic discussion. This
+#'   is provided using a call to \code{get_theme_topics} or
+#'   \code{get_themes_topics}
+#'
+#' @return A tibble containing the topic discussions for selected topic/s
+#'
+#' @examples
+#' links <- get_theme_topics(link = get_themes()$links[4])
+#' get_topics_discussion(link = links)
+#'
+#' @export
 #'
 #'
 #'
 #
 ################################################################################
 
-get_topic_question <- function(link) {
-  page <- xml2::read_html(x = link)
-
-
-
-  topic <- page %>%
-    rvest::html_nodes(css = "#pagebody h1") %>%
-    rvest::html_text()
-
-
+get_topics_discussion <- function(links) {
+  ## Get vector of links
+  topicLinks <- links$Link
 
   ##
-  question <- page %>%
-    rvest::html_nodes(css = "#pagebody #cphContent_pnlQuestion .body") %>%
-    rvest::html_text() %>%
-    stringr::str_split(pattern = "\r\n\r\n|\r\n") %>%
-    unlist()
+  discussions <- lapply(X = topicLinks,
+                        FUN = get_topic_discussion) %>%
+    dplyr::bind_rows()
 
+  ## Return discussions
+  return(discussions)
 }
-
-
-page %>% rvest::html_nodes(css = "#pagebody .post")
-
-page %>% rvest::html_nodes(css = "#pagebody .post .data h3")
-
-page %>% rvest::html_nodes(css = "#pagebody .post .data .datadetail")
-
-page %>% rvest::html_nodes(css = "#pagebody .post .body")
