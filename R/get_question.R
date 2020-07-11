7e71bdac8723635f10f7ab991bfcfbd5998558e7
Ernest Guevarra <ernest@guevarra.io>
2020-07-11 08:35:23 +0100

    create new function

---
+++ R/get_question.R
@@ -0,0 +1,33 @@
+################################################################################
+#
+#'
+#' Get the question for a particular topic
+#'
+#' @param link URL for topic discussion
+#'
+#' @return A tibble containing the topic question
+#'
+#' @examples
+#' links <- get_theme_topics(link = get_themes()$links[4])
+#' get_topic_question(link = links$Link)
+#'
+#'
+#'
+#
+################################################################################
+
+get_topic_question <- function(link) {
+  page <- xml2::read_html(x = link[1])
+
+  topic <- page %>%
+    rvest::html_nodes(css = "#pagebody h1") %>%
+    rvest::html_text()
+
+  ##
+  question <- page %>%
+    rvest::html_nodes(css = "#pagebody #cphContent_pnlQuestion .body") %>%
+    rvest::html_text() %>%
+    stringr::str_split(pattern = "\r\n\r\n|\r\n") %>%
+    unlist()
+
+}
