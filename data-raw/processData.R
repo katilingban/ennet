93844161e83fedc1b411b16832d5d24117ee9691
Ernest Guevarra <ernest@guevarra.io>
2020-10-04 21:49:14 +0100

    edit processData.R

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -1,6 +1,12 @@
 ##
+#x <- retrieve_ennet()
+#ennet_dataset <- x
 
-x <- retrieve_ennet()
-ennet_dataset <- x
+#usethis::use_data(ennet_dataset, overwrite = TRUE, compress = "xz")
 
-usethis::use_data(ennet_dataset, overwrite = TRUE, compress = "xz")
+
+## Create topics dataset
+x <- get_themes() %>% get_themes_topics()
+ennet_topics <- x
+
+usethis::use_data(ennet_topics, overwrite = TRUE, compress = "xz")
