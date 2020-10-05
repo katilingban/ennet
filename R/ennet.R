2c7e73862fed4731b25224372c74ad5f7c148be6
Ernest Guevarra <ernest@guevarra.io>
2020-10-05 11:00:04 +0100

    update global variables and dependencies

--- R/ennet.R
+++ R/ennet.R
@@ -18,14 +18,16 @@
 #' @importFrom rvest html_nodes html_attr html_text
 #' @importFrom xml2 read_html url_absolute
 #' @importFrom tibble tibble
-#' @importFrom dplyr bind_rows mutate first
+#' @importFrom dplyr bind_rows mutate first group_by count ungroup
 #' @importFrom stringr str_replace str_detect str_extract_all str_to_lower
 #'   str_replace_na str_remove_all
-#' @importFrom lubridate dmy_hm
+#' @importFrom lubridate dmy_hm month year
 #'
 #
 ################################################################################
 "_PACKAGE"
 
-## quiets concerns of R CMD check re: Posted
-if(getRversion() >= "2.15.1")  utils::globalVariables("Posted")
+## quiets concerns of R CMD check on global variables
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("Posted", "Month",
+                                                        "Theme", "Views",
+                                                        "Year"))
