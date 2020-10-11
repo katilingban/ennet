e34fb45cccb1373268b58ad525506315cd732b66
Ernest Guevarra <ernest@guevarra.io>
2020-10-11 07:31:45 +0100

    create ennet_topics dataset

---
+++ R/data.R
@@ -0,0 +1,27 @@
+################################################################################
+#
+#'
+#' en-net topics
+#'
+#' @format A tibble with 2980 rows and 7 columns:
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | `Theme` | Thematic areas in the en-net forum |
+#' | `Topic` | Short description of the topic/question being discussed/raised |
+#' | `Views` | Number of views of the topic/question being discussed/raised |
+#' | `Author` | Name of person who raised the topic/question |
+#' | `Posted` | Date topic/question was posted on en-net forum |
+#' | `Link` | URL of the topic/question being discussed/raised |
+#' | `Replies` | Number of replies to the topic/question being discussed/raised |
+#'
+#' @examples
+#' ennet_topics
+#'
+#' @source
+#' https://www.en-net.org
+#'
+#'
+#
+################################################################################
+"ennet_topics"
