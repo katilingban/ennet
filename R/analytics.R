aada1d9abda4af532206aae75d5ac63eb2d06599
Ernest Guevarra <ernest@guevarra.io>
2020-10-17 20:44:27 +0100

    update function and package documentation

--- R/analytics.R
+++ R/analytics.R
@@ -5,8 +5,10 @@
 #'
 #' @param topics A tibble of topics by theme from en-net forum produced through
 #'   a call to `get_themes_topics`.
-#' @param by_date Should topics be counted by month of the year or just by
-#'   year? Default is by month of the year.
+#' @param by_date Should topics be grouped by month of the year or just by
+#'   year or overall? Default is to group by month of the year.
+#' @param .sort Logical. Should output be sorted by count frequencies? Default
+#'   is TRUE.
 #'
 #' @return A tibble of topic counts by theme and by specified date format
 #'
@@ -21,7 +23,9 @@
 #
 ################################################################################
 
-count_topics <- function(topics, by_date = c("month_year", "year")) {
+count_topics <- function(topics,
+                         by_date = c("month_year", "year", "all"),
+                         .sort = TRUE) {
   by_date <- match.arg(by_date)
   x <- topics %>%
     dplyr::mutate(Month = factor(x = month.abb[lubridate::month(Posted)],
@@ -32,15 +36,45 @@ count_topics <- function(topics, by_date = c("month_year", "year")) {
   ## by_date == "month_year"
   if(by_date == "month_year") {
     x <- x %>%
-      dplyr::count(Month, Year) %>%
-      dplyr::ungroup()
+      dplyr::count(Month, Year)
+
+    ## check if need to sort
+    if(.sort) {
+      x <- x %>%
+        dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+    }
+
+    ## ungroup
+    x <- x %>% dplyr::ungroup()
   }
 
   ## by_date == "year"
   if(by_date == "year") {
     x <- x %>%
-      dplyr::count(Year) %>%
-      dplyr::ungroup()
+      dplyr::count(Year)
+
+    ## check if need to sort
+    if(.sort) {
+      x <- x %>%
+        dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+    }
+
+    ## ungroup
+    x <- x %>% dplyr::ungroup()
+  }
+
+  ## by_date == "all"
+  if(by_date == "all") {
+    x <- x %>%
+      dplyr::count()
+
+    ## check if need to sort
+    if(.sort) {
+      x <- x %>% dplyr::arrange(dplyr::desc(n))
+    }
+
+    ## ungroup
+    x <- x %>% dplyr::ungroup()
   }
 
   ## Return
@@ -109,10 +143,10 @@ arrange_views <- function(topics,
     }
   }
 
-  ## Arrange by descening number of views
+  ## Arrange by descending number of views
   x <- x %>%
-    dplyr::arrange(dplyr::desc(Views), .by_group = TRUE) #%>%
-    #dplyr::ungroup()
+    dplyr::arrange(dplyr::desc(Views), .by_group = TRUE) %>%
+    dplyr::ungroup()
 
   ## Return
   return(x)
@@ -171,8 +205,8 @@ arrange_replies <- function(topics,
   ## Arrange by descening number of views
   x <- x %>%
     dplyr::arrange(dplyr::desc(Replies), .by_group = TRUE) %>%
-    dplyr::filter(!is.na(Replies)) #%>%
-    #dplyr::ungroup()
+    dplyr::filter(!is.na(Replies)) %>%
+    dplyr::ungroup()
 
   ## Return
   return(x)
@@ -189,6 +223,8 @@ arrange_replies <- function(topics,
 #' @param by_theme Logical. If TRUE (default), count by theme.
 #' @param by_date Should topics be counted by month of the year or just by
 #'   year or total? Default is to count by month of the year.
+#' @param .sort Logical. Should output be sorted by count frequencies? Default
+#'   is TRUE.
 #'
 #' @return A tibble of topic counts by author, by theme and by specified date
 #'   format
@@ -206,7 +242,8 @@ arrange_replies <- function(topics,
 
 count_authors <- function(topics,
                           by_theme = TRUE,
-                          by_date = c("month_year", "year", "all")) {
+                          by_date = c("month_year", "year", "all"),
+                          .sort = TRUE) {
   by_date <- match.arg(by_date)
   x <- topics %>%
     dplyr::mutate(Month = factor(x = month.abb[lubridate::month(Posted)],
@@ -218,38 +255,77 @@ count_authors <- function(topics,
     if(by_date == "all") {
       x <- x %>%
         dplyr::count(Theme, Author)
+
+      ## check if need to sort
+      if(.sort) {
+        x <- x %>%
+          dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+      }
     }
 
     ## by_date == "month_year"
     if(by_date == "month_year") {
       x <- x %>%
         dplyr::count(Theme, Author, Month, Year)
+
+      ## check if need to sort
+      if(.sort) {
+        x <- x %>%
+          dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+      }
     }
 
     ## by_date == "year"
     if(by_date == "year") {
       x <- x %>%
         dplyr::count(Theme, Author, Year)
+
+      ## check if need to sort
+      if(.sort) {
+        x <- x %>%
+          dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+      }
     }
   } else {
     ## by_date == "all"
     if(by_date == "all") {
       x <- x %>%
         dplyr::count(Author)
+
+      ## check if need to sort
+      if(.sort) {
+        x <- x %>%
+          dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+      }
     }
 
     ## by_date == "month_year"
     if(by_date == "month_year") {
       x <- x %>%
         dplyr::count(Author, Month, Year)
+
+      ## check if need to sort
+      if(.sort) {
+        x <- x %>%
+          dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+      }
     }
 
     ## by_date == "year"
     if(by_date == "year") {
       x <- x %>%
         dplyr::count(Author, Year)
+
+      ## check if need to sort
+      if(.sort) {
+        x <- x %>%
+          dplyr::arrange(dplyr::desc(n), .by_group = TRUE)
+      }
     }
   }
+
+  ## ungroup
+  #x <- x %>% dplyr::ungroup()
 
   ## Return
   return(x)
