a2c9b05b9e86c5900885ca048667ea091358501a
Ernest Guevarra <ernest@guevarra.io>
2020-11-25 09:29:54 +0000

    remove non-ASCII strings

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -1,5 +1,6 @@
 ## Load libraries
 library(magrittr)
+library(stringr)
 
 ## Create themes dataset
 x <- get_themes()
@@ -10,6 +11,29 @@ usethis::use_data(ennet_themes, overwrite = TRUE, compress = "xz")
 
 ## Create topics dataset
 x <- get_themes() %>% get_themes_topics()
+
+## replace UTF-8 strings
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "é|ê|è|ë", replacement = "e")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "É", replacement = "E")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "à", replacement = "a")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "ü", replacement = "u")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "Ñ|ñ", replacement = "n")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "œ", replacement = "oe")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "Ó", replacement = "o")
+#x$Topic <- stringr::str_replace_all(string = x$Topic,
+#                                    pattern = "ó", replacement = "o")
+
+x <- x %>%
+  mutate(Topic = iconv(x$Topic, from = "UTF-8", to = "ASCII"),
+         Author = iconv(x$Author, from = "UTF-8", to = "ASCII"))
+
 ennet_topics <- x
 
 usethis::use_data(ennet_topics, overwrite = TRUE, compress = "xz")
