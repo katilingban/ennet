48d12442dd19f136e8af72fb7ab793ae77fe6665
Ernest Guevarra <ernest@guevarra.io>
2021-01-16 18:17:38 +0000

    add tests for database functions

---
+++ tests/testthat/test-database.R
@@ -0,0 +1,23 @@
+
+x <- get_db_discussions()
+
+test_that("x is a tibble", {
+  expect_is(x, "tbl")
+})
+
+
+fn <- c("ennet_topics_2021-01-16_00:58:05.csv",
+        "ennet_topics_2021-01-16_02:47:04.csv",
+        "ennet_topics_2021-01-16_03:59:22.csv")
+
+x <- create_db_topics_daily(.date = Sys.Date(), fn = fn)
+
+test_that("x is a tibble", {
+  expect_is(x, "tbl")
+})
+
+x <- create_db_topics_monthly(.date = Sys.Date())
+
+test_that("x is a tibble", {
+  expect_is(x, "tbl")
+})
